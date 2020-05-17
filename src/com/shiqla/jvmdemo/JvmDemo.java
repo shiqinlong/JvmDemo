272aa430c5a8f688b78de4bc79569f72a7f38466
qinlong.shi <qinlong.shi@sap.com>
2020-05-17 17:44:23 +0800

    create test class

---
+++ src/com/shiqla/jvmdemo/JvmDemo.java
@@ -0,0 +1,32 @@
+package com.shiqla.jvmdemo;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃  神兽保佑
+ * 　　　　┃　　　┃  代码无bug
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * Desc
+ * Auth c5285333
+ * Date 2020-05-17
+ */
+public class JvmDemo
+{
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
