a8a4f0a309d42159d547c7b103116215eefc60bb
ShiQinlong <503434169@qq.com>
2020-05-18 22:23:36 +0800

    jvm p45

---
+++ src/com/shiqla/jvmdemo/chapter02_runtimedataarea/PCRegister.java
@@ -0,0 +1,28 @@
+package com.shiqla.jvmdemo.chapter02_runtimedataarea;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃  神兽保佑
+ * 　　　　┃　　　┃  代码无bug
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * Desc
+ * Auth Administrator
+ * Date 2020/5/18 0018
+ */
+public class PCRegister {
+}
---
+++ src/com/shiqla/jvmdemo/chapter02_runtimedataarea/runTimeDataArea.java
@@ -0,0 +1,32 @@
+package com.shiqla.jvmdemo.runtimedataarea;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃  神兽保佑
+ * 　　　　┃　　　┃  代码无bug
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * Desc
+ * Auth Administrator
+ * Date 2020/5/18 0018
+ */
+public class runTimeDataArea {
+
+    public static void main(String[] args) {
+        
+    }
+}
