908f5b1f848305ef93c530b85db5261841c1ff0a
qinlong.shi <qinlong.shi@sap.com>
2020-05-20 21:52:11 +0800

    added content of stack fram

---
+++ src/com/shiqla/jvmdemo/chapter02_runtimedataarea/LocalVariables.java
@@ -0,0 +1,40 @@
+package com.shiqla.jvmdemo.chapter02_runtimedataarea;
+
+import java.util.Date;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃  神兽保佑
+ * 　　　　┃　　　┃  代码无bug
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * Desc
+ * Auth c5285333
+ * Date 2020-05-20
+ */
+public class LocalVariables
+{
+    public void test(String str){
+        int i=0;
+        int j=30;
+        Date date = new Date();
+    }
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/com/shiqla/jvmdemo/chapter02_runtimedataarea/StackFrame.java
@@ -0,0 +1,29 @@
+package com.shiqla.jvmdemo.chapter02_runtimedataarea;
+
+/**
+ * Desc
+ * Auth c5285333
+ * Date 2020-05-20
+ */
+public class StackFrame
+{
+
+
+    public void method3(){
+
+    }
+
+    public void method2(){
+
+    }
+
+    public void mtehod1(){
+
+    }
+
+    public static void main (String[] args)
+    {
+
+
+    }
+}
