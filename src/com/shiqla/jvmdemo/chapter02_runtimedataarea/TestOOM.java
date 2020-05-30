9e28df26e21c76ad1aba653d18acbb133a27dc96
qinlong.shi <qinlong.shi@sap.com>
2020-05-30 23:02:36 +0800

    gc

---
+++ src/com/shiqla/jvmdemo/chapter02_runtimedataarea/TestOOM.java
@@ -0,0 +1,47 @@
+package com.shiqla.jvmdemo.chapter02_runtimedataarea;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃  神兽保佑
+ * 　　　　┃　　　┃  代码无bug
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * Desc ${DESC}
+ * Auth c5285333
+ * Date 2020-05-29
+ */
+public class TestOOM {
+
+    public static void main(String[] args) throws InterruptedException {
+        byte[] buffer;
+
+        List list = new LinkedList();
+
+        for(int i=0;i<1000;i++){
+            buffer = new byte[1024*1024*5];
+            list.add(buffer);
+            Thread.sleep(1000*4);
+//            if( i%5 == 0){
+//                ((LinkedList) list).removeFirst();
+//            }
+        }
+    }
+}
