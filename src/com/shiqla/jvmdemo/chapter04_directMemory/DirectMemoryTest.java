cc0260e80332a0c931c4789fed66faab8f5d6e78
qinlong.shi <qinlong.shi@sap.com>
2020-06-02 22:27:17 +0800

    118 execution engine

--- src/com/shiqla/jvmdemo/chapter04_directMemory/DirectMemoryTest.java
+++ src/com/shiqla/jvmdemo/chapter04_directMemory/DirectMemoryTest.java
@@ -1,5 +1,9 @@
 package com.shiqla.jvmdemo.chapter04_directMemory;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * ━━━━━━神兽出没━━━━━━
  * 　　　┏┓　　　┏┓
@@ -25,7 +29,36 @@ package com.shiqla.jvmdemo.chapter04_directMemory;
  * Date 2020-06-01
  */
 public class DirectMemoryTest {
+
+    public static final int size = 1024*1024*100;
+
     public static void main(String[] args){
-        
+        DirectMemoryTest.directMemoryTest();
+    }
+
+
+    /**
+     * the direct memory area will be throws a oom exception
+     * java.lang.OutOfMemoryError: Direct buffer memory
+     */
+    public static void directMemoryTest() {
+
+        List list = new ArrayList<>();
+        int count = 0;
+        try {
+            while (true) {
+
+                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(size);
+                count++;
+                list.add(byteBuffer);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } finally {
+            System.out.println(count);
+        }
     }
 }
---
+++ src/com/shiqla/jvmdemo/chapter05_executionengine/ExecutionEngineTest.java
@@ -0,0 +1,43 @@
+package com.shiqla.jvmdemo.chapter05_executionengine;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃  神兽保佑
+ * 　　　　┃　　　┃  代码无bug
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * Desc ${DESC}
+ * Auth c5285333
+ * Date 2020-06-02
+ */
+public class ExecutionEngineTest {
+
+    public static String name = "shi";
+
+    static{
+        System.out.println("static shiqla");
+    }
+
+    public static void main(String[] args){
+        int i = 0;
+        System.out.println(i);
+    }
+
+    public void test(){
+        System.out.println("test");
+    }
+}
