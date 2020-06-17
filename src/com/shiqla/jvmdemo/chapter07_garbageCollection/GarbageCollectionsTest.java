cd59db406d620429a5daab485b3df068649b28fd
qinlong.shi <qinlong.shi@sap.com>
2020-06-17 21:35:19 +0800

    garbage Collection

--- src/com/shiqla/jvmdemo/chapter07_garbageCollection/GarbageCollectionsTest.java
+++ src/com/shiqla/jvmdemo/chapter07_garbageCollection/GarbageCollectionsTest.java
@@ -2,6 +2,10 @@ package com.shiqla.jvmdemo.chapter07_garbageCollection;
 
 import org.junit.Test;
 
+import javax.sound.midi.Soundbank;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Desc ${DESC}
  * Auth c5285333
@@ -14,7 +18,6 @@ public class GarbageCollectionsTest {
 
     private byte[] bytes = new byte[20*1024*1024];
 
-    @Test
     public void finalzation_test_01(){
 
         garbageCollectionsTest = new GarbageCollectionsTest();
@@ -30,5 +33,28 @@ public class GarbageCollectionsTest {
 
         System.out.println("对象被收回");
         super.finalize();
+    }
+
+    @Test
+    public void threadPoolTest(){
+
+        ExecutorService executors = Executors.newFixedThreadPool(100);
+
+        for (int i=0;i<100;i++){
+            executors.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1000*500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        System.out.println("test");
+
+
     }
 }
