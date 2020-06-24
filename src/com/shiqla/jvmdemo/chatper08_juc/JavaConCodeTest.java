2eec9388cd1a178ad865df057694ebc0ff248ca0
qinlong.shi <qinlong.shi@sap.com>
2020-06-24 09:29:50 +0800

    juc

--- src/com/shiqla/jvmdemo/chatper08_juc/JavaConCodeTest.java
+++ src/com/shiqla/jvmdemo/chatper08_juc/JavaConCodeTest.java
@@ -1,9 +1,155 @@
 package com.shiqla.jvmdemo.chatper08_juc;
 
+import org.junit.Test;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Desc ${DESC}
  * Auth c5285333
  * Date 2020-06-19
  */
 public class JavaConCodeTest {
+
+    public ReentrantLock reentrantLock = new ReentrantLock();
+
+
+    @Test
+    public void print_test(){
+        Lock lock = new ReentrantLock(true);
+
+        Thread threadA = new Thread(new PrintThread(lock),"A");
+        Thread threadB = new Thread(new PrintThread(lock),"B");
+        Thread threadC = new Thread(new PrintThread(lock),"C");
+        threadA.start();
+        threadB.start();
+        threadC.start();
+
+    }
+
+    public void threadPool_test(){
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+
+
+    public void testCallableThread(){
+        FutureTask<String> futureTask = new FutureTask<>(new CallableThread());
+
+        try {
+            System.out.println("main thread id: " + Thread.currentThread().getId());
+
+            new Thread(futureTask,"shiqla").start();
+
+            System.out.println("results: " + futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void concurrentHashMap_Test01() {
+        reentrantLock.lock();
+        System.out.println("test");
+        test001();
+        reentrantLock.unlock();
+    }
+
+
+    public synchronized void test001(){
+        System.out.println("test");
+    }
+
+
+    public void TestVolatile(){
+        TestVolatile testVolatile = new TestVolatile();
+        Thread thread = new Thread(testVolatile);
+        thread.start();
+
+        while(true){
+            if(testVolatile.isFlag()){
+                System.out.println("----------");
+                break;
+            }
+        }
+
+
+        Thread t1 = new Thread(new TestVolatile(){
+            public void run(){
+                System.out.println("nihao");
+            }
+        });
+
+        t1.start();
+    }
 }
+
+class TestVolatile implements Runnable{
+
+    private volatile boolean flag = false;
+
+    private volatile AtomicInteger num = new AtomicInteger(0);
+
+    @Override
+    public void run() {
+
+        num.incrementAndGet();
+        num.addAndGet(1024);
+        this.flag = true;
+
+        System.out.println("flag = " + this.flag);
+    }
+
+    public boolean isFlag(){
+        return this.flag;
+    }
+
+}
+
+class CallableThread implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+
+//        Thread.sleep(50*1000);
+        return "thread name : " + Thread.currentThread().getName() ;
+    }
+}
+
+class PrintThread implements Runnable{
+
+
+    private Lock lock;
+
+    public PrintThread(Lock lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0;i<5;i++){
+            try {
+                lock.lock();
+                System.out.println(Thread.currentThread().getName());
+            }finally {
+                lock.unlock();
+            }
+
+        }
+
+    }
+}
+
+
+
--- src/com/shiqla/jvmdemo/chatper08_juc/juc.md
+++ src/com/shiqla/jvmdemo/chatper08_juc/juc.md
@@ -1,1 +1,15 @@
-####Volatile
+##Java Concurrent Code
+ #### Volatile 内存可见性
+    使用此关键字修饰的变量，操作会在主存中进行即无线程的缓存，直接读取主存的值，在修改之后直接刷新主存的值
+    不具备互斥性
+    无法保证原子性
+ #### 原子变量，可以保证线程安全
+    采用 volatile 保证内存可见性
+    采用 Cas 算法保证原子性，
+ ####ConcurrentHashMap 线程安全的 hashmap
+ #### 创建线程方式
+    继承Thread类 重写run 方法
+    实现runnable 接口，实现run 方法
+    实现callable 接口，实现call方法
+        有返回值，切可以跑出异常
+    线程池创建线程
