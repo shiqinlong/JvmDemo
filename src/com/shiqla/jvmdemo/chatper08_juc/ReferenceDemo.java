f3773382b0dd3b3aaeeb9e5e6e42b39bef98d361
qinlong.shi <qinlong.shi@sap.com>
2020-07-06 20:50:08 +0800

    update reference queue

---
+++ src/com/shiqla/jvmdemo/chatper08_juc/ReferenceDemo.java
@@ -0,0 +1,20 @@
+package com.shiqla.jvmdemo.chatper08_juc;
+
+import sun.jvm.hotspot.memory.StringTable;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
+/**
+ * Desc ${DESC}
+ * Auth c5285333
+ * Date 2020-07-05
+ */
+public class ReferenceDemo {
+
+    public static void main(String[] args){
+
+    }
+}
