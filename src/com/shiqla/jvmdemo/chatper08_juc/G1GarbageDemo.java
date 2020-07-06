99585b1d9513feaad99d016edb9be024f3109b9f
qinlong.shi <qinlong.shi@sap.com>
2020-07-06 22:15:05 +0800

    linux command

---
+++ src/com/shiqla/jvmdemo/chatper08_juc/G1GarbageDemo.java
@@ -0,0 +1,18 @@
+package com.shiqla.jvmdemo.chatper08_juc;
+
+/**
+ * Desc ${DESC}
+ * Auth c5285333
+ * Date 2020-07-06
+ */
+public class G1GarbageDemo {
+    
+    public static void main(String[] args){
+
+
+        for (;;){
+            Object object = new Object();
+        }
+
+    }
+}
